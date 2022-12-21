22df14bd7d268d424197b7ced04fbf3cf7b75b23
headbwoi <tochukwujohn24@gmail.com>
2022-12-21 17:06:56 +0100

    start base bot

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import { Express } from "express"
 import express from "express"
 import { connectDB } from "./config/db.js"
 import "dotenv/config.js"
-connectDB()
+// connectDB()
 
 const port = process.env.PORT || 5000
 
 const app: Express = express()
 
-app.get("/", (req, res) => res.send("Hello World!"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  let prompt: {} = req.body.text
+  if (prompt.toString().includes("name")) {
+    res.send("Hello my name is bot")
+  } else {
+    res.send("i dont recognise the command")
+  }
+  //   res.send("enter name")
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
