7dbbcb9e68cd02f08d3bb03c60783f8b49c75b49
XeuxDev <tochukwujohn24@gmail.com>
2022-12-23 16:00:49 +0100

    yeah

--- frontend/src/components/Layout/InputForm.tsx
+++ frontend/src/components/Layout/InputForm.tsx
@@ -1,11 +1,23 @@
 const InputForm = () => {
   return (
-    <div className="absolute bottom-0 left-0 w-full h-14 -mb-14">
+    <div className="absolute bottom-0 left-0 w-full h-14 -mb-14 px-4 flex items-center justify-between">
       <input
         type="text"
-        className="w-full h-full outline-none bg-black px-4 text-white placeholder:text-gray-400"
+        className="w-full h-full pr-5 outline-none bg-black text-white placeholder:text-gray-400"
         placeholder="Enter text"
       />
+      <div className="w-7 h-7 rounded-full bg-gray-700 grid place-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+          className="w-4 h-4 text-white"
+        >
+          <path
+            d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
     </div>
   )
 }
