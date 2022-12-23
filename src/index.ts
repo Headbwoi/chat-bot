b263e9b3f33bda492c5922f4ea81d30e3249c335
XeuxDev <tochukwujohn24@gmail.com>
2022-12-23 11:28:12 +0100

    adds cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config.js"
 connectDB()
 import botRouter from "./routes/botRoute.js"
 import { errorHandler } from "./middleware/errorMiddleware.js"
+import cors from "cors"
 
 const port = process.env.PORT || 5000
 
@@ -12,6 +13,12 @@ const app: Express = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+)
 
 app.use("/", botRouter)
 
