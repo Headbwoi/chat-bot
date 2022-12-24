b4c532586860bcda57bbfbe8bca9390e5855236c
headbwoi <tochukwujohn24@gmail.com>
2022-12-24 14:33:03 +0100

    adds seed data

--- backend/dist/seedDB/data.js
+++ backend/dist/seedDB/data.js
@@ -1,18 +1,22 @@
 export const dataSet = [
     {
-        question: "what can you do what do you can what are your capabilities what are you capable of",
-        answer: "You can ask me about BUK",
+        question: "what can you do, what do you do, what are your capabilities, what are you capable of",
+        answer: "I am a dumb bot but but you can ask me about BUk",
     },
     {
         question: "where is fcsit",
         answer: "go f yourself",
     },
     {
-        question: "who is the vc",
-        answer: "sagir something i dunno",
+        question: "who is the vc, where is the vc, what is the vc, where does the vc stay ",
+        answer: "The Vice Chancellor stays at the Senate building",
     },
     {
         question: "Hi Hello Holla wassup hey start begin initiate init",
-        answer: "Hi, Nice to have you, How can i help you?",
+        answer: "Welcome, How can i help you",
+    },
+    {
+        question: "who created you, who made you, who is your boss, who is your engineer",
+        answer: "My Supreme Master goes by the Alias Xeux, He is very smart as he made me in 2 days.",
     },
 ];
