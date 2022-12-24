927d6761549907c7e211cf4fc69aee2364d1f0a8
headbwoi <tochukwujohn24@gmail.com>
2022-12-24 15:10:00 +0100

    modified UI

--- frontend/src/components/Chat/UserChat.tsx
+++ frontend/src/components/Chat/UserChat.tsx
@@ -7,7 +7,7 @@ const UserChat = () => {
     <div
       className={`right-0 absolute bottom-0 w-fit ${
         userText === "" ? "" : "bg-gray-800"
-      } p-4 mb-36 md:mb-28 max-w-[60%] break-words h-auto rounded-md`}
+      } p-4 mb-48 md:mb-28 max-w-[60%] break-words h-auto rounded-md`}
     >
       {userText}
     </div>
