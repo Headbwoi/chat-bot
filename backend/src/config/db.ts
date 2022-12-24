53d5fc2e6ae54293753cd6de6a281fa67913ec67
headbwoi <tochukwujohn24@gmail.com>
2022-12-24 15:36:21 +0100

    fixed mongo url

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
   try {
     await mongoose
-      .connect(`${process.env.MONGO_URL_LOCAL}`)
+      .connect(`${process.env.MONGO_URL}`)
       .then(() => console.log(`Database mongodb🚀🚀 connected `))
   } catch (error) {
     console.log(error)
