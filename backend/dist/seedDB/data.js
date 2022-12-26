0bd9c93b7341f3ffbc41e4c3f28b64fe04f59f2b
headbwoi <tochukwujohn24@gmail.com>
2022-12-26 23:28:08 +0100

    adds seed data

--- backend/dist/seedDB/data.js
+++ backend/dist/seedDB/data.js
@@ -4,12 +4,12 @@ export const dataSet = [
         answer: "I am a dumb bot but but you can ask me about BUk",
     },
     {
-        question: "where is fcsit",
-        answer: "go f yourself",
+        question: "where is Foc, FOC, fcsit, FCSIT",
+        answer: "The Faculty of Computing is located in BUK new Site campus",
     },
     {
         question: "who is the vc, where is the vc, what is the vc, where does the vc stay ",
-        answer: "The Vice Chancellor stays at the Senate building",
+        answer: "The Vice Chancellor's Office is located at the Senate building",
     },
     {
         question: "Hi Hello Holla wassup hey start begin initiate init",
@@ -18,5 +18,13 @@ export const dataSet = [
     {
         question: "who created you, who made you, who is your boss, who is your engineer",
         answer: "My Supreme Master goes by the Alias Xeux, He is very smart as he made me in 2 days.",
+    },
+    {
+        question: "shit, fuck, motherfucker, fool, stupid, idiot",
+        answer: "Please don't use curse words for me, i am dumb doesn't mean i won't learn over time.",
+    },
+    {
+        question: "BUK",
+        answer: "Bayero University Kano is the 4th Best University in Nigeria, the Best in Northern Nigeria.",
     },
 ];
